a0415e5179b1904a81038bd93f80a320081f4110
Anastasia Samilyk <asamilyk@yandex.ru>
2024-03-27 22:15:08 +0300

    add initial design for main page

--- mysite/app/views.py
+++ mysite/app/views.py
@@ -4,9 +4,9 @@ from django.shortcuts import render
 def upload_file(request):
     if request.method == 'POST':
         if 'file_upload' not in request.FILES:
-            return render(request, 'shopapp/first_page.html')
+            return render(request, 'app/first_page2.html')
         uploaded_file = request.FILES['file_upload']
         # Здесь вы можете выполнить дополнительные действия с загруженным файлом, например, сохранить его на сервере
-        return render(request, 'shopapp/result_page.html', {'uploaded_file': uploaded_file})
-    return render(request, 'shopapp/first_page.html')
+        return render(request, 'app/result_page.html', {'uploaded_file': uploaded_file})
+    return render(request, 'app/index.html')
 
