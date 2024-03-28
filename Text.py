09cbf36dcf265ccedecc3d8ba9b58095afba5092
Anastasia Samilyk <asamilyk@yandex.ru>
2024-03-28 18:49:35 +0300

    add generalStatistics method to class Text

--- Text.py
+++ Text.py
@@ -1,11 +1,18 @@
 import re
+import nltk
 from nltk.corpus import stopwords
+from collections import Counter
 
+
+# nltk.download('punkt')
+# nltk.download('averaged_perceptron_tagger_ru')
 
 
 class Text:
     text = ""
     tokenized_text = []
+    part_of_speech_stat_num = {}
+    part_of_speech_stat_per = {}
 
     def __init__(self, text):
         self.text = text
@@ -17,10 +24,43 @@ class Text:
         t = t.lower().split()
         self.tokenized_text = t
 
+    def generalStatistics(self):
+        pos_tags = self.get_pos_tags()
+        pos_counts = Counter(tag for word, tag in pos_tags)
+        print("Количество частей речи в тексте:")
+        tags = {'Наречие': "ADV",
+                'Предлог': "PR",
+                'Прилагательное в женском роде': "A=f",
+                'Прилагательное в мужском роде': "A=m",
+                'Прилагательное в среднем роде': "A=n",
+                'Прилагательное во множественном числе роде': "A=pl",
+                'Личные местоимения': "S-PRO",
+                'Глаголы': "V",
+                'Союзы': "CONJ",
+                'Притяжательное прилательное м.р.': "A-PRO=m",
+                'Притяжательное прилательное ж.р.': "A-PRO=f",
+                'Притяжательное прилательное ср.р.': "A-PRO=n",
+                'Притяжательное прилательное мн.ч.': "A-PRO=pl",
+                'Наречие-местоимение': "ADV-PRO",
+                'Числительное м.р.': "NUM=m",
+                'Числительное и.п.': "NUM=nom",
+                'Прилагательное': "A",
+                'Вводное': "PARENTH"}
+        total = sum(pos_counts.values())
+        for key in tags.keys():
+            count = pos_counts[tags[key]]
+            self.part_of_speech_stat_num[key] = count
+            self.part_of_speech_stat_per[key] = float(count) * 100 / total
+        self.part_of_speech_stat_num = dict(
+            sorted(self.part_of_speech_stat_num.items(), key=lambda item: item[1], reverse=True))
+        self.part_of_speech_stat_per = dict(
+            sorted(self.part_of_speech_stat_per.items(), key=lambda item: item[1], reverse=True))
+
+    def get_pos_tags(self):
+        tokens = nltk.word_tokenize(" ".join(self.tokenized_text), language='russian')
+        pos_tags = nltk.pos_tag(tokens, lang='rus')
+        return pos_tags
+
     def deleteStopWords(self):
         stop_words = set(stopwords.words('russian'))
         print(stop_words)
-
-
-
-
--- main.py
+++ main.py
@@ -4,7 +4,11 @@ import Text
 
 t = poem.TEXT
 obj = Text.Text(t)
-obj.deleteStopWords()
+obj.tokenize()
+print(obj.tokenized_text)
+obj.generalStatistics()
+print(obj.part_of_speech_stat_num)
+print(obj.part_of_speech_stat_per)
 
 
 
